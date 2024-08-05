71c673abf55354d1b03d5fdd76a06a5db3c9408a
Sabra Akhavan <100771060+Sabraaa@users.noreply.github.com>
2024-08-05 16:43:34 +0330

    working with default values in arrrays

--- data structure/script.js
+++ data structure/script.js
@@ -33,9 +33,11 @@ const restaurant = {
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 const [i, , [j, k]] = nested;
-      console.log(i, j, k);
+console.log(i, j, k);
 
-
+//default values
+const [p=1, q=1, r=1] = [8, 9];
+console.log(p, q, r);
 
 // const arr = [1, 2, 3];
 // const a = arr[0];
