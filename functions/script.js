344b2c2b02dfcfab5877a6ce50ac9c72d3ca8eb0
Sabra Akhavan <100771060+Sabraaa@users.noreply.github.com>
2024-08-15 18:14:05 +0330

    passing arguments value vs. refrences

--- functions/script.js
+++ functions/script.js
@@ -1,15 +1,29 @@
 'use strict';
 
 const bookings = [];
-const createBooking = function (flightNum, numPassengers=1, price=199) {
-
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
   const booking = {
     flightNum,
     numPassengers,
     price,
   };
-  console.log(booking);
+  //   console.log(booking);
   bookings.push(booking);
 };
 
 createBooking('LH233');
+const flight = 'LH234';
+const sabra = {
+  name: 'sabra akhavan',
+  passport: 123456789,
+};
+const checkIn = function (flightNum, passenger) {
+    flightNum = "LH999"
+    passenger.name="ms. " + passenger.name
+    if (passenger.passport===123456789) {
+        alert('Check in')
+    }else[
+        alert('Wrong passport')
+    ]
+}
+checkIn(flight, sabra)
