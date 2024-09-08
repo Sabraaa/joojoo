d2a62ac5bf48525a04b75849e4d573836c86c728
Sabra Akhavan <100771060+Sabraaa@users.noreply.github.com>
2024-09-08 19:41:40 +0330

    sending req to API with XML data structure

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -6,3 +6,10 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const req = new XMLHttpRequest();
+req.open('GEt', 'https://restcountries.com/v2/name/portugal');
+req.send();
+
+req.addEventListener('load', function(){
+    const [data] = JSON.parse(this.responseText)
+    console.log(data)
+})
